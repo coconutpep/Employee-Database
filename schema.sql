fd4b8cacd062e43b2d0b42cbc8408174d00d0e67
unknown <ryanklueg@gmail.com>
2020-03-28 12:00:44 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -18,16 +18,18 @@ CREATE TABLE dept_manager(
 	from_date VARCHAR(10) NOT NULL,
 	to_date VARCHAR(10),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date VARCHAR(10) NOT NULL,
 	to_date VARCHAR(10),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles(
