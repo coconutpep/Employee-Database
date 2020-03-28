606b75eac10820d30b0e330f0b24020cc7528225
unknown <ryanklueg@gmail.com>
2020-03-28 12:06:34 -0400

    view employees hired in 1986

--- query.sql
+++ query.sql
@@ -1,4 +1,10 @@
+--View Employee number, name, gender, and salary
 SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
 FROM employees AS e
 JOIN salaries AS s
 ON e.emp_no=s.emp_no;
+
+--List employees who were hired in 1986
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '1986%';
