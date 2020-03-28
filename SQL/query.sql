2d52748e7b56f447b7fe0fe153b416ed04b71533
unknown <ryanklueg@gmail.com>
2020-03-28 12:57:43 -0400

    list to view employees and their dept

--- SQL/query.sql
+++ SQL/query.sql
@@ -14,3 +14,9 @@ SELECT m.emp_no, m.dept_no, d.dept_name, e.first_name, e.last_name,  m.from_date
 FROM dept_manager AS m
 JOIN departments AS d ON m.dept_no=d.dept_no
 JOIN employees AS e ON m.emp_no=e.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name
+SELECT de.emp_no, e.first_name, e.last_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e ON de.emp_no=e.emp_no
+JOIN departments AS d ON de.dept_no=d.dept_no;
